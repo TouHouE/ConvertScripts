42deac6e264fc6639f0010a6329bec65680a83b3
TouHouE <hsu.william9@gmail.com>
2024-05-20 14:51:21 +0800

    Remove useless variable and command `bash -c`

--- convert_hsinchu.sh
+++ convert_hsinchu.sh
@@ -7,20 +7,13 @@
 #SBATCH --output=%x/%j.out
 #SBATCH --cpus-per-task=32
 
-export MASTER_ADDR=$(scontrol show hostnames $SLURM_JOB_NODELIST | head -n 1)
-export MASTER_PORT=$(( RANDOM % (50000 - 30000 + 1 ) + 30000 ))
-export GPUS_PER_NODE=$SLURM_GPUS_PER_NODE
-export NNODES=$SLURM_NNODES
-export NUM_PROCESSES=$(expr $NNODES \* $GPUS_PER_NODE)
 export HSU="/mnt/home/hsu.william9"
 export SHARE="/mnt/share/NTU_Heart_CT/"
-export JSON_LIST_IN_CONTAINER="/workspace/data/ct.json"
-export DATA_IN_CONTAINER="/workspace/data/"
 export CONTAINER_NAME="$HSU/workspace/dimage/nemo_monai.sqsh"
 export SRC_MNT="$SHARE/usbD/RAW/batch2-2024-04-30/CCTA2022全年+2024.1.1-2024.4.24(232例):/workspace/src-232"
 export DST_MNT="$SHARE/data:/workspace/data"
 export PROJ_MNT="$HSU/workspace/project:/workspace/project"
-export CONTAINER_MOUNT="$SRC_MNT,$DST_MNT,$PROJ_MNT"
+export CONTAINER_MOUNT="${SRC_MNT},${DST_MNT},${PROJ_MNT}"
 
 export PYTHON_CMD="
 python /workspace/project/ConvertScripts/dcm2niix_HsinChu.py \
@@ -29,4 +22,4 @@ python /workspace/project/ConvertScripts/dcm2niix_HsinChu.py \
 --dcm2niix=/workspace/project/ConvertScripts/lib/dcm2niix --num_workers=16
 "
 
-srun --container-image=$CONTAINER_NAME --container-mounts=$CONTAINER_MOUNT --container-writable bash -c $PYTHON_CMD
+srun --container-image=$CONTAINER_NAME --container-mounts=$CONTAINER_MOUNT --container-writable $PYTHON_CMD
