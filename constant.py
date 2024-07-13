9bbe2160722f26a3ed65be028a1683c7a46e54a3
TouHouE <hsu.william9@gmail.com>
2024-07-13 18:18:46 +0800

    refactor: adding hint type and prompt scripts usage constant table

--- constant.py
+++ constant.py
@@ -1,5 +1,5 @@
 STATUS_LEN: int = 15
-DIGIT2LABEL_NAME = {
+DIGIT2LABEL_NAME: dict[int, str] = {
     1: 'RightAtrium',
     2: 'RightVentricle',
     3: 'LeftAtrium',
@@ -11,4 +11,11 @@ DIGIT2LABEL_NAME = {
     9: 'Bypass',
     10: 'Plaque'
 }
-LABEL_NAME2DIGIT = {value: key for key, value in DIGIT2LABEL_NAME.items()}
+LABEL_NAME2DIGIT: dict[str, int] = {value: key for key, value in DIGIT2LABEL_NAME.items()}
+EXPAND_SHORT_NAME: dict[str, str] = {
+    'LM': "Left main coronary artery",
+    "LAD": "Left anterior descending artery",
+    "LCX": "Left circumflex artery",
+    "RCA": "Right coronary artery"
+}
+
