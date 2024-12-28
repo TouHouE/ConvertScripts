c5f02f06175785f72cb8ca3ca31e270fc8f223ca
TouHouE <hsu.william9@gmail.com>
2024-12-29 05:06:25 +0800

    feat(`Detail`, `SegmentMetaPack`): easier to manage image_mask_plq pair doc: rewrite some type-hint, try to consider `field`

--- models/CommonDataModels.py
+++ models/CommonDataModels.py
@@ -2,23 +2,40 @@ import json
 import argparse
 import multiprocessing as mp
 from dataclasses import dataclass, field
-from typing import Iterable, List, Optional
-
+from typing import Iterable, List, Optional, Sequence, Union, TypeVar, Generic
+import os
 import numpy as np
 
 from utils import convert_utils as CUtils
 
 __all__ = [
-    'Partition', 'IspCtPair', 'DebugCard'
+    'Partition', 'IspCtPair', 'DebugCard', 'SegmentMetaPack'
 ]
+T = TypeVar('T')
+
+
 @dataclass
-class Partition:
+class Partition(Generic[T]):
+    """
+        This class using to package all of multi-processing scripts needed data. It contains:
+        Attributes:
+            proc_id: The id of the current process
+            data: An iterable of waiting for each processing processed.
+            args: handle arguments come from command-line
+            Optional attributes:
+            out_dir: The directory to save the output(*.nii.gz & *.json come from dcm2niix.exe) to.
+            buf_dir: The directory to save the buffer(*.dcm file) to.
+            err_dir: The directory to save the error message to.
+            PID: same as `proc_id`
+            patient_list: same as `data`
+
+    """
     proc_id: int
-    data: list | np.ndarray | Iterable
+    data: Iterable[T]
     args: argparse.Namespace
-    out_dir: str = field(default='./out')
-    buf_dir: str = field(default='./buf')
-    err_dir: str = field(default='./err')
+    out_dir: str = field(default='out')
+    buf_dir: str = field(default='buf')
+    err_dir: str = field(default='err')
 
     def __post_init__(self):
         if self.out_dir is None:
@@ -33,7 +50,7 @@ class Partition:
         return self.proc_id
 
     @property
-    def patient_list(self) -> list | np.ndarray | Iterable:
+    def patient_list(self) -> Sequence[Iterable]:
         return self.data
 
 @dataclass
@@ -43,6 +60,28 @@ class IspCtPair:
     pid: str
     uid: str
     mask: str
+
+
+@dataclass(frozen=True, eq=True, order=True)
+class Detail:
+    path: str | os.PathLike = field(hash=True)
+    vessel: str | os.PathLike = field(hash=True)
+    desc: str = field(hash=True)
+
+
+@dataclass
+class SegmentMetaPack:
+    cp: float | int
+    pid: str
+    uid: str
+    image: str | os.PathLike
+    mask: str | os.PathLike
+    plaque: Optional[str | os.PathLike] = field(default=None)
+    details: Optional[Sequence[Detail | dict]] = field(default=None)
+
+    def __post_init__(self):
+        if self.details is not None:
+            self.details = tuple(sorted([Detail(**_detail) for _detail in self.details]))
 
 
 class DebugCard:
